d1d4c14e88f51d5068cd2720186e6e8fdfb1645b
Oliver Widder <oliver.widder@iteratec.de>
2015-09-21 00:18:02 +0200

    Mon Sep 21 00:18:02 CEST 2015

--- node/readAllImages.js
+++ node/readAllImages.js
@@ -1,18 +1,30 @@
 'use strict';
 
-var request=require("request");
+var request = require("request");
+var Q = require("q");
 
-var options = {
-    url: "http://geek-and-poke.com",
-    headers: {
-        "User-Agent": "request"
-    }
-};
+function getFirstLink() {
+    var defer = Q.defer();
 
-request.get(options, function(error, response){
-    if(error){
-        console.log(error);
-    }else{
-        console.log(response.body);
-    }
+    var options = {
+        url: "http://geek-and-poke.com",
+        headers: {
+            "User-Agent": "request"
+        }
+    };
+
+    request.get(options, function(error, response) {
+        if(error) {
+            console.log(error);
+        } else {
+            defer.resolve(response.body);
+        }
+    });
+
+    return defer.promise;
+}
+
+getFirstLink().then(function(result) {
+   console.log(result);
 });
+
