7fceef57a49295fa136a9cf7a55f9b6a437e8d12
Oliver Widder <oliver.widder@iteratec.de>
2015-09-28 19:34:48 +0200

    Mon Sep 28 19:34:48 CEST 2015

--- angular/js/controller/picGridController.js
+++ angular/js/controller/picGridController.js
@@ -5,7 +5,11 @@ angular.module(__global.appName).controller("picGridController", function ($scop
     var ALL_IMAGES_URL = "../node/allImages.txt";
     var IMAGE_FOLDER = "../node/images/";
 
+    var INITIAL_NUMBER_OF_IMAGES = 20;
+    var NUMBER_OF_IMAGES_TO_LOAD = 10;
+
     var posts;
+    var indices;
 
     function readImages() {
         var deferred = $q.defer();
@@ -32,6 +36,10 @@ angular.module(__global.appName).controller("picGridController", function ($scop
         return deferred.promise;
     }
 
+    function getTitleFromIndex(index) {
+        return posts[index].title;
+    }
+
     function getImageUrlFromIndex(index) {
         return IMAGE_FOLDER + posts[index].filename;
     }
@@ -40,23 +48,31 @@ angular.module(__global.appName).controller("picGridController", function ($scop
         return posts[index].pathToPage;
     }
 
-    readImages().then(function() {
-        $scope.images = [];
-        for(var i = 0; i < 50; i++) {
-            $scope.images.push(i);
+    function init() {
+        indices = [];
+        for(var i = 0; i < INITIAL_NUMBER_OF_IMAGES; i++) {
+            indices.push(i);
         }
-        $scope.loadMore = function () {
-            var last = $scope.images[$scope.images.length - 1];
-            if(last < posts.length - 1) {
-                for (var i = 1; i <= 8; i++) {
-                    if(last + i < posts.length) {
-                        $scope.images.push(last + i);
-                    }
+    }
+
+    function loadMore() {
+        var lastIndex = indices[indices.length - 1];
+        if(lastIndex < posts.length - 1) {
+            for (var i = 1; i <= NUMBER_OF_IMAGES_TO_LOAD; i++) {
+                if(lastIndex + i < posts.length) {
+                    indices.push(lastIndex + i);
                 }
             }
-        };
+        }
+    }
+
+    readImages().then(function() {
+        init();
+        $scope.indices = indices;
+        $scope.loadMore = loadMore;
 
         $scope.getImageUrlFromIndex = getImageUrlFromIndex;
         $scope.getPostPathFromIndex = getPostPathFromIndex;
+        $scope.getTitleFromIndex = getTitleFromIndex;
     });
 });
