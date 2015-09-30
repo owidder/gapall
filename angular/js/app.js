63f757424f84cff2d7c75052e5229dc01573742a
Oliver Widder <oliver.widder@iteratec.de>
2015-09-30 18:11:21 +0200

    index-dev

--- angular/js/app.js
+++ angular/js/app.js
@@ -4,10 +4,6 @@
     var app = angular.module(__global.appName, ["ngRoute", "infinite-scroll"]);
     app.config(function ($routeProvider) {
         $routeProvider
-            .when("/force", {
-                templateUrl: "html/views/componentsView.html",
-                controller: "ComponentsController"
-            })
             .when("/picgrid", {
                 templateUrl: "html/views/picGridView.html",
                 controller: "picGridController"
