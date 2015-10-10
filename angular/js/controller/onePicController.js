619dbb4f7f168bd91d9412065aa13a3272e8a63e
Oliver Widder <oliver.widder@iteratec.de>
2015-10-10 10:03:31 +0200

    one pic view

--- angular/js/controller/onePicController.js
+++ angular/js/controller/onePicController.js
@@ -1,10 +1,17 @@
 'use strict';
 
-angular.module(__global.appName).controller("onePicController", function($scope, gapImages) {
+angular.module(__global.appName).controller("onePicController", function($scope, $route, gapImages) {
+
+    function reload() {
+        $route.reload();
+    }
 
     gapImages.ready.then(function(data) {
-        var index = Math.floor(Math.random() * gapImages.count);
+        var index = Math.floor(Math.random() * gapImages.count());
         $scope.url = gapImages.getImageUrlFromIndex(index);
         $scope.title = gapImages.getTitleFromIndex(index);
+        $scope.postUrl = gapImages.getPostPathFromIndex(index);
+        $scope.date = gapImages.getDateFromIndex(index);
+        $scope.reload = reload;
     });
 });
