5cb1af2f6a02720f0e44c304999ae8f5226975cd
Oliver Widder <oliver.widder@iteratec.de>
2015-10-11 10:11:20 +0200

    url parameter "index"

--- angular/js/controller/onePicController.js
+++ angular/js/controller/onePicController.js
@@ -1,13 +1,19 @@
 'use strict';
 
-angular.module(__global.appName).controller("onePicController", function($scope, $route, gapImages) {
+angular.module(__global.appName).controller("onePicController", function($scope, $route, $location, gapImages) {
+    var INDEX_PARAM = "index";
 
     function reload() {
+        $location.search(INDEX_PARAM, "");
         $route.reload();
     }
 
     gapImages.ready.then(function(data) {
-        var index = Math.floor(Math.random() * gapImages.count());
+        var index = parseInt($location.search()[INDEX_PARAM]);
+        if(!(index > 0)) {
+            index = Math.floor(Math.random() * gapImages.count());
+            $location.search("index", index);
+        }
 
         $scope.index = index;
         $scope.reload = reload;
