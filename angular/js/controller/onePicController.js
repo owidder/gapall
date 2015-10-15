6a9e36a7c73393d3f42e6802cbd678676e46e15d
Oliver Widder <oliver.widder@iteratec.de>
2015-10-16 01:14:40 +0200

    play & stop

--- angular/js/controller/onePicController.js
+++ angular/js/controller/onePicController.js
@@ -1,16 +1,46 @@
 'use strict';
 
-angular.module(__global.appName).controller("onePicController", function($scope, $route, $location, gapImages, util) {
+angular.module(__global.appName).controller("onePicController", function($scope, $route, $location, $timeout, gapImages, util) {
     var DATE_PARAM = "date";
+    var PLAY_PARAM = "play";
+    var PLAY_ICON = "play_arrow";
+    var STOP_ICON = "stop";
 
-    function reload() {
+    var playPromise;
+    var playStopIcon = PLAY_ICON;
+
+    function random() {
         $location.search(DATE_PARAM, "");
         $route.reload();
+    }
+
+    function playStop() {
+        if(playStopIcon == PLAY_ICON) {
+            playStopIcon = STOP_ICON;
+            play();
+        }
+        else {
+            playStopIcon = PLAY_ICON;
+            stop();
+        }
+    }
+
+    function play() {
+        $location.search(PLAY_PARAM, 1);
+        $route.reload();
+    }
+
+    function stop() {
+        if(util.isSet(playPromise)) {
+            $timeout.cancel(playPromise);
+        }
+        $location.search(PLAY_PARAM, 0);
     }
 
     gapImages.ready.then(function(data) {
         var index = 0;
         var date = $location.search()[DATE_PARAM];
+        var play = $location.search()[PLAY_PARAM];
 
         if(util.isNotEmpty(date)) {
             index = gapImages.getIndexFromDate(date);
@@ -21,7 +51,19 @@ angular.module(__global.appName).controller("onePicController", function($scope,
             $location.search(DATE_PARAM, date);
         }
 
+        if(play == 1) {
+            playPromise = $timeout(function() {
+                playPromise = undefined;
+                random();
+            }, 10000);
+        }
+        else {
+            playStopIcon = "play_arrow";
+        }
+
         $scope.index = index;
-        $scope.reload = reload;
+        $scope.random = random;
+        $scope.play = play;
+        $scope.stop = stop;
     });
 });
